7b49e8a6c9345b8183a63916fd8e0b72a5385700
pashqa87 <pashqa87@yandex.ru>
2022-03-10 17:31:35 +0300

    feat: compleat carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function dateSample(sampleActivity) {
+  if (sampleActivity < 0 || sampleActivity > 15) return false
+  if (typeof sampleActivity !== 'string') return false
+  if (!parseFloat(sampleActivity)) return false
+  return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / (0.693 / HALF_LIFE_PERIOD))
 }
+
 
 module.exports = {
   dateSample
